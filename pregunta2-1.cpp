95b5d4ce1cb5e369a652d46ec53b4233be540907
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-04-30 13:04:05 -0500

    Create pregunta2-1.cpp

---
+++ pregunta2-1.cpp
@@ -0,0 +1,53 @@
+//INSERT SORT
+#include<ctime>
+#include <bits/stdc++.h> 
+using namespace std; 
+
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n) 
+{ 
+	int i, key, j; 
+	for (i = 1; i < n; i++) 
+	{ 
+		key = arr[i]; 
+		j = i - 1; 
+
+		/* Move elements of arr[0..i-1], that are 
+		greater than key, to one position ahead 
+		of their current position */
+		while (j >= 0 && arr[j] > key) 
+		{ 
+			arr[j + 1] = arr[j]; 
+			j = j - 1; 
+		} 
+		arr[j + 1] = key; 
+	} 
+} 
+
+// A utility function to print an array of size n 
+void printArray(int arr[], int n) 
+{ 
+	int i; 
+	for (i = 0; i < n; i++) 
+		cout << arr[i] << " "; 
+	cout << endl; 
+} 
+
+/* Driver code */
+int main() 
+{ 
+    clock_t t;
+    int arr[] = { 12, 11, 13, 5, 6 }; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+
+    t=clock();
+	  insertionSort(arr, n); 
+    t=clock()-t;
+
+	  printArray(arr, n); 
+
+    cout<<"El tiempo fue de "<<float(t)/CLOCKS_PER_SEC<<"segundos"<<endl;
+	  return 0; 
+} 
+
+//Fuente:geeksforgeeks
