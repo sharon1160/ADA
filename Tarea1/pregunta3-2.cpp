ab1f1856af329f4b3953a0a8ab08f196e5270077
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-04-30 16:37:11 -0500

    Update pregunta3-2.cpp

--- Tarea1/pregunta3-2.cpp
+++ Tarea1/pregunta3-2.cpp
@@ -13,38 +13,39 @@ void swap(int *xp, int *yp)
 // A function to implement bubble sort 
 void bubbleSort(int arr[], int n) 
 { 
-    int asig=0;//# de asignaciones
-    int compa=0;//# de comparaciones
-    int objeto=0;//# de creaciones de objetos
-    int memoria=0;//# de veces que separa memoria para un arreglo o lista de tamanho 'n' 
+	int asig=0;//# de asignaciones
+	int compa=0;//# de comparaciones
+	int objeto=0;//# de creaciones de objetos
+	int memoria=0;//# de veces que separa memoria para un arreglo o lista de tamanho 'n' 
 
 	int i, j; 
-  objeto+=2;//por la creacion de los enteros i y j
-  asig++;//por el valor cero que toma i en el for
+	objeto+=2;//por la creacion de los enteros i y j
+	asig++;//por el valor cero que toma i en el for
 	for (i = 0; i < n-1; i++,asig++){//el asig++ por el i++	 
-         // Last i elements are already in place
-        compa++;//comparacion del for
-        asig++;//por el valor cero que toma j en el for
-        for (j = 0; j < n-i-1; j++,asig++){//el asig++ por el i++
-            compa+=2;//comparacion del for y del if
-            if (arr[j] > arr[j+1]) 
-                swap(&arr[j], &arr[j+1]);
-                asig+=3;//las asignaciones que hay dentro del swap
-                objeto++;//la creacion de un entero dentro del swap
-        }
-        compa++;//la ultima comparacion que no cumple la condicion del for
-    }
-    compa++;//la ultima comparacion que no cumple la condicion del for
+		 // Last i elements are already in place
+		compa++;//comparacion del for
+		asig++;//por el valor cero que toma j en el for
+		for (j = 0; j < n-i-1; j++,asig++){//el asig++ por el i++
+		    compa+=2;//comparacion del for y del if
+		    if (arr[j] > arr[j+1]){ 
+			swap(&arr[j], &arr[j+1]);
+			asig+=3;//las asignaciones que hay dentro del swap
+			objeto++;//la creacion de un entero dentro del swap
+		    }
+		}
+		compa++;//la ultima comparacion que no cumple la condicion del for
+	}
+	compa++;//la ultima comparacion que no cumple la condicion del for
 
-    asig=asig*8;
-    compa=compa*2;
-    objeto=objeto*200;
-    //memoria=memoria*50 + n* 10
+	asig=asig*8;
+	compa=compa*2;
+	objeto=objeto*200;
+	//memoria=memoria*50 + n* 10
 
-    cout<<"Numero de asignaciones: "<<asig<<endl;
-    cout<<"Numero de comparaciones: "<<compa<<endl;
-    cout<<"Numero de creaciones de objeto: "<<objeto<<endl;
-    cout<<"Numero de asignaciones de memoria: "<<memoria<<endl;
+	cout<<"Numero de asignaciones: "<<asig<<endl;
+	cout<<"Numero de comparaciones: "<<compa<<endl;
+	cout<<"Numero de creaciones de objeto: "<<objeto<<endl;
+	cout<<"Numero de asignaciones de memoria: "<<memoria<<endl;
 } 
 
 /* Function to print an array */
