9f23f14d7ac632a45e3ea2c3d6104b8662f0624c
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-05-15 16:15:36 -0500

    Update Ejercicio1.py

--- Practica03/Ejercicio1.py
+++ Practica03/Ejercicio1.py
@@ -27,37 +27,25 @@ def merge_sort(array, left_index, right_index):
     merge(array, left_index, right_index, middle)
 
 def merge(array, left_index, right_index, middle):
-    # Make copies of both arrays we're trying to merge
 
-    # The second parameter is non-inclusive, so we have to increase by 1
     left_copy = array[left_index:middle + 1]
     right_copy = array[middle+1:right_index+1]
 
-    # Initial values for variables that we use to keep
-    # track of where we are in each array
     left_copy_index = 0
     right_copy_index = 0
     sorted_index = left_index
 
-    # Go through both copies until we run out of elements in one
     while left_copy_index < len(left_copy) and right_copy_index < len(right_copy):
 
-        # If our left_copy has the smaller element, put it in the sorted
-        # part and then move forward in left_copy (by increasing the pointer)
         if left_copy[left_copy_index] <= right_copy[right_copy_index]:
             array[sorted_index] = left_copy[left_copy_index]
             left_copy_index = left_copy_index + 1
-        # Opposite from above
         else:
             array[sorted_index] = right_copy[right_copy_index]
             right_copy_index = right_copy_index + 1
 
-        # Regardless of where we got our element from
-        # move forward in the sorted part
         sorted_index = sorted_index + 1
 
-    # We ran out of elements either in left_copy or right_copy
-    # so we will go through the remaining elements and add them
     while left_copy_index < len(left_copy):
         array[sorted_index] = left_copy[left_copy_index]
         left_copy_index = left_copy_index + 1
@@ -73,25 +61,12 @@ def mostrar(A):
     for i in range (len(A)):
         print(A[i])
         
-def creciente(tamanio):
-    return [x for x in range(1, tamanio+1)]
-
-def decreciente(tamanio):
-    return [x for x in range(tamanio, 0, -1)]
-
 def aleatorio(tamanio):
     return [random.randint(0, tamanio) for x in range(tamanio)]
 
-
 def generador(elec,tamanio):
     array=[]
-    if elec == "creciente":
-        array=creciente(tamanio)
-        return array
-    elif elec == "decreciente":
-        array=decreciente(tamanio)
-        return array
-    elif elec == "aleatorio":
+    if elec == "aleatorio":
         array=aleatorio(tamanio)
         return array
 
@@ -112,10 +87,7 @@ def dividir(B):#divide cada elemento entre el numero de veces que se tomo el tie
 #tamanios = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000]
 #tamanios = [25, 50, 75, 100, 125, 150, 175,200]
 tamanios = [40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-print("*Creciente")
-print("*Decreciente")
-print("*Aleatorio")
-elec=str(input("Escribe tu opcion :"))
+elec="aleatorio"
 #### Promedio Merge Sort #######
 PromedioTimeMerge = []
 #### Promedio Insert Sort #######
@@ -167,5 +139,3 @@ ax2.set_title("Comparación de Merge e Insert Sort en arreglos tipo " + elec)
 ax2.legend()
 ax2.grid()
 fig2.savefig(elec + "_tiempo_promedio_real.png")
-
-
