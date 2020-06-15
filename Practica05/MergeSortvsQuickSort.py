0c522aae0208a0e99e84abd35a186acd8c9503df
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-06-14 20:14:05 -0500

    Update MergeSortvsQuickSort.py

--- Practica05/MergeSortvsQuickSort.py
+++ Practica05/MergeSortvsQuickSort.py
@@ -6,47 +6,39 @@ import time
 
 ########## QUICKSORT ##########
 
-def partition(arr,low,high): 
-	i = ( low-1 )		 # index of smaller element 
-	pivot = arr[high]	 # pivot 
-
-	for j in range(low , high): 
-
-		# If current element is smaller than or 
-		# equal to pivot 
-		if arr[j] <= pivot: 
-		
-			# increment index of smaller element 
-			i = i+1
-			arr[i],arr[j] = arr[j],arr[i] 
-
-	arr[i+1],arr[high] = arr[high],arr[i+1] 
-	return ( i+1 ) 
-
-# The main function that implements QuickSort 
-# arr[] --> Array to be sorted, 
-# low --> Starting index, 
-# high --> Ending index 
-
-# Function to do Quick sort 
-def Quick_Sort(arr,low,high): 
-	if low < high: 
-
-		# pi is partitioning index, arr[p] is now 
-		# at right place 
-		pi = partition(arr,low,high) 
-
-		# Separately sort elements before 
-		# partition and after partition 
-		Quick_Sort(arr, low, pi-1) 
-		Quick_Sort(arr, pi+1, high) 
-
-# Driver code to test above 
-# arr = [10, 7, 8, 9, 1, 5] 
-# n = len(arr) 
-# quickSort(arr,0,n-1) 
-# print(arr)
-
+def Quick_Sort(L, first, last):
+    # definimos los índices y calculamos el pivote
+    i = first
+    j = last    
+    pivote = (L[i] + L[j]) / 2
+    # iteramos hasta que i no sea menor que j
+    while i < j:
+        # iteramos mientras que el valor de L[i] sea menor que pivote
+        while L[i] < pivote:
+            # Incrementamos el índice
+            i+=1
+        # iteramos mientras que el valor de L[j] sea mayor que pivote
+        while L[j] > pivote:
+            # decrementamos el índice
+            j-=1
+        # si i es menor o igual que j significa que los índices se han cruzado
+        if i <= j:
+            # creamos una variable temporal para guardar el valor de L[j]
+            x = L[j]
+            # intercambiamos los valores de L[j] y L[i]
+            L[j] = L[i]
+            L[i] = x
+            # incrementamos y decrementamos i y j respectivamente
+            i+=1
+            j-=1
+    # si first es menor que j mantenemos la recursividad
+    if first < j:
+        L = Quick_Sort(L, first, j)
+    # si last es mayor que i mantenemos la recursividad
+    if last > i:
+        L = Quick_Sort(L, i, last)
+    # devolvemos la lista ordenada
+    return L
 
 ############ MERGESORT (INTERCALA) ###########
 
@@ -90,7 +82,7 @@ def aleatorio(tamanio):
 ########## MAIN ##########
 
 tamanios = []
-for i in range (1000,105001,1000):
+for i in range (1000,100001,1000):
     tamanios.append(i)
 
 
