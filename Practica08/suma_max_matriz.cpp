c3ac356582030686f883b57369899f2355caccd8
Sharon Rossely Chullunquía <schullunquia@unsa.edu.pe>
2020-07-19 14:58:09 -0500

    Agregando comentarios

--- Practica08/suma_max_matriz.cpp
+++ Practica08/suma_max_matriz.cpp
@@ -15,6 +15,8 @@ int sumAcum(int **A,int i,int j){
         return(sumAcum(A,i-1,j)+A[i][j]);
     }
     if (i!=0 && j !=0){
+        //A[i,j] = sumatoria de los elementos hasta el indice i,j
+        //        = Orig[i,j] + A[i,j-1] + A[i-1,j] - A[i-1,j-1]
         return (A[i][j] + sumAcum(A,i,j-1)+ sumAcum(A,i-1,j)-sumAcum(A,i-1,j-1));
     }
 }
@@ -45,6 +47,7 @@ int sumaMatriz(int **A,int fInf,int cInf,int fSup ,int  cSup){
         return resultado;
     }
     if (cInf != 0 & fInf != 0){
+        //resultado = z - p - q + r
         resultado = sumAcum(A,fSup,cSup) - sumAcum(A,fInf-1,cSup) - sumAcum(A,fSup,cInf-1) + sumAcum(A,fInf-1,cInf-1);
         return resultado;
     }
